95ec8c5192e2323fd254470870220305f57b16ea
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-24 15:33:27 +0100

    Ajout du tableau Show

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,26 @@
-require 'router'
-require 'boardcase'
+require_relative 'router'
+require_relative 'boardcase'
 
 class Board
 
+  def initializeb(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+  
+  puts "          ______________________________  "
+  puts "         /_____________________________/| "
+  puts "        |         |         |         | | "  
+  puts "    ____|    A    |    B    |    C    | | "
+  puts "   /____|_________|_________|_________| | "
+  puts "  |     |         |         |         | | "
+  puts "  |  1  |  #{a1}  |  #{a2}  |  #{a3}  | | "  
+  puts "  |_____|_________|_________|_________| | "
+  puts "  |     |         |         |         | | "
+  puts "  |  2  |  #{b1}  |  #{b2}  |  #{b3}  | | "
+  puts "  |_____|_________|_________|_________| | "
+  puts "  |     |         |         |         | | "
+  puts "  |  3  |  #{c1}  |  #{c2}  |  #{c3}  | | "
+  puts "  |_____|_________|_________|_________|/  "
+  puts "                                          "  
+
+  end
 
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,54 @@
-class Game 
+require_relative 'board'
+require_relative 'app'
+require 'pry'
 
-  def initialize 
+$:.unshift File.expand_path("./../lib", __FILE__)
+
+class Game
+
+
+
+  def initialize(board)
     @board = Board.new
   end
 
-  def usr(player_one, player_two)
-    @player_one = user1
-    @player_two = user2  
-  end
+
+  # def users ( player_one, player_two )
+  #   @player_one = user1
+  #   @player_two = user2  
+  # end
+
+
+  def perform
+    puts "BIENVENUE DANS LE MORPION INSOLITE !"
+    while true
+
+      #on affiche le menu
+      puts "Quel est le prénom du joueur 1 ?"
+      user1 = gets.chomp.to_i
+      puts "Quel est le prénom du joueur 2 ?"
+      user2 = gets.chomp.to_i
+
+      @player_one = user1
+      @player_two = user2  
+
+      binding.pry
+
+
+      # case params
+      # when 1
+      #   puts "Très bien on va créer un gossip"
+      #   @controller.create_gossip
+      # when 4
+      #   puts "Asta la vista ! "
+      #   break
+      # else 
+      #   puts "Ce choix n'existe pas, re-essayes !"
+      # end
+    end
+  end 
+
+
+
 end
 
